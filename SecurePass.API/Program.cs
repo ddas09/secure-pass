da44191daca5e9dc249b8594f5be6fadb24a10b0
Dipankar Das <dipankar.das@intimetec.com>
2024-01-21 10:40:57 +0530

    Day-4: Configured JWT auth, hangfire and email

--- SecurePass.API/Program.cs
+++ SecurePass.API/Program.cs
@@ -1,7 +1,16 @@
+using Hangfire;
+using System.Text;
 using SecurePass.Data;
+using Hangfire.SqlServer;
 using Microsoft.AspNetCore.Mvc;
 using SecurePass.Common.Models;
+using SecurePass.API.Extensions;
+using SecurePass.DAL.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SecurePass.Services.Extensions;
+using Hangfire.Dashboard.BasicAuthorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +30,7 @@ builder.Services.AddSwaggerGen();
 // For generating lowecase routes
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
+// Adding DB context
 builder.Services.AddDbContext<SecurePassDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SecurePass"));
@@ -37,19 +47,98 @@ builder.Services.Configure<MailConfiguration>
         builder.Configuration.GetSection(nameof(MailConfiguration))
     );
 
+// For registering services for DI
+builder.Services.RegisterServices();
+
+// For registering repositories for DI
+builder.Services.RegisterRepositories();
+
+// For registering action filters
+builder.Services.RegisterActionFilters();
+
+// For AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Hanfire Configurations
+builder.Services.AddHangfire(configuration => configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(builder.Configuration.GetConnectionString("SecurePass"), new SqlServerStorageOptions
+    {
+        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+        QueuePollInterval = TimeSpan.Zero,
+        UseRecommendedIsolationLevel = true,
+        DisableGlobalLocks = true
+    }));
+
+builder.Services.AddHangfireServer();
+
+// For configuring CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+// For configuring JWT Authentication
+var jwtConfiguration = builder.Configuration.GetSection(nameof(JwtConfiguration)).Get<JwtConfiguration>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters()
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfiguration.AccessTokenSecret)),
+        ValidIssuer = jwtConfiguration.Issuer,
+        ValidAudience = jwtConfiguration.Audience,
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHangfireDashboard("/hangfire/dashboard", new DashboardOptions
+    {
+        Authorization = new[] { new BasicAuthAuthorizationFilter(new BasicAuthAuthorizationFilterOptions
+        {
+            RequireSsl = false,
+            SslRedirect = false,
+            LoginCaseSensitive = true,
+            Users = new []
+            {
+                new BasicAuthAuthorizationUser
+                {
+                    Login = builder.Configuration.GetSection(nameof(Hangfire)).GetValue(typeof(string), "AdminUser").ToString(),
+                    PasswordClear =  builder.Configuration.GetSection(nameof(Hangfire)).GetValue(typeof(string), "BasicAuthPass").ToString()
+                },
+            }
+        })},
+        IgnoreAntiforgeryToken = true
+    });
 }
+
+// For configuring custom middlewares
+app.ConfigureMiddlewares();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
